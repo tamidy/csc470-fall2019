afdccc37ae7c255460d679319f84a1b75c337122
csc470-fall2019 <yousafit@yahoo.com>
2019-09-19 17:24:55 -0400

    updated projected

--- exercises/Plane/Plane/Assets/Scripts/PlaneScript.cs
+++ exercises/Plane/Plane/Assets/Scripts/PlaneScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlaneScript : MonoBehaviour
 {
+    /*No physics, no forward key, just transform.forward by itself, floating in space, left and right keys for rotations*/
+
+    private Rigidbody rb; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
